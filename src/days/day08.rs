153d9deb11883fa27949fd5cd3d3935f932842a0
JakeHuneau <jakehuneau@yahoo.com>
2023-12-09 11:18:47 -0500

    Day9

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,7 +1,90 @@
 #![allow(unused)]
 use crate::{Solution, SolutionPair};
+use num::integer;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
+#[derive(Debug, Clone)]
+enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Debug, Clone)]
+struct Pair {
+    left: String,
+    right: String,
+}
+
+impl Pair {
+    fn traverse(&self, direction: Direction) -> String {
+        match direction {
+            Direction::Left => self.left.clone(),
+            Direction::Right => self.right.clone(),
+        }
+    }
+}
+
 pub fn solve() -> SolutionPair {
-    todo!()
+    let input = read_to_string("input/day8").unwrap();
+    let parts = input.split("\n\n").collect::<Vec<&str>>();
+    let instructions: Vec<Direction> = parts
+        .first()
+        .unwrap()
+        .chars()
+        .map(|ch| match ch {
+            'L' => Direction::Left,
+            'R' => Direction::Right,
+            _ => panic!("Unknown direction"),
+        })
+        .collect();
+    let mut nodes: HashMap<String, Pair> = HashMap::new();
+    parts.last().unwrap().lines().for_each(|l| {
+        let line_parts = l.split(" = ").collect::<Vec<&str>>();
+        let parent = line_parts.first().unwrap().to_string();
+        let binding = line_parts.last().unwrap().replace("(", "").replace(")", "");
+        let children = binding.split(",").collect::<Vec<&str>>();
+        let left = children.first().unwrap().trim().to_string();
+        let right = children.last().unwrap().trim().to_string();
+        nodes.insert(parent, Pair { left, right });
+    });
+    let mut steps = 0;
+    let mut current_node = String::from("AAA");
+    'outer: loop {
+        for direction in &instructions {
+            steps += 1;
+            current_node = nodes
+                .get(&current_node)
+                .unwrap()
+                .traverse(direction.clone());
+            if current_node == String::from("ZZZ") {
+                break 'outer;
+            }
+        }
+    }
+
+    let part_2_solution = nodes
+        .clone()
+        .into_keys()
+        .filter(|parent| parent.ends_with("A"))
+        .map(|node| {
+            let mut current_node = node.clone();
+            let mut steps = 0;
+            'outer: loop {
+                for direction in &instructions {
+                    steps += 1;
+                    current_node = nodes
+                        .get(&current_node)
+                        .unwrap()
+                        .traverse(direction.clone());
+                    if current_node.ends_with("Z") {
+                        break 'outer;
+                    }
+                }
+            }
+            steps
+        })
+        .fold(1, integer::lcm);
+
+    (Solution::UInt(steps), Solution::UInt(part_2_solution))
 }
--- src/days/day09.rs
+++ src/days/day09.rs
@@ -2,6 +2,55 @@
 use crate::{Solution, SolutionPair};
 use std::fs::read_to_string;
 
+fn extrapolate_next_value(values: Vec<isize>) -> (isize, isize) {
+    /// Given a number sequence, figure out the next value.
+    /// Do this by finding differences until all 0's, then
+    /// work way back up.
+    /// Ex: 0   3   6   9  12  15
+    ///  Goes to
+    /// 0   3   6   9  12  15  [18]
+    ///   3   3   3   3   3   [3]
+    ///     0   0   0   0   0
+    /// Returns: (extrapolated before value, after value)
+    // First work way down
+    let mut tower: Vec<Vec<isize>> = vec![values.clone()];
+    let mut current_values = values;
+    loop {
+        let differences: Vec<isize> = current_values
+            .windows(2)
+            .map(|pair| pair[1] - pair[0])
+            .collect();
+        if differences.iter().all(|&v| v == 0) {
+            break;
+        } else {
+            tower.push(differences.clone());
+            current_values = differences;
+        }
+    }
+
+    // Now work way up
+    let mut current_diff_end = tower.last().unwrap().last().unwrap().clone();
+    let mut current_diff_start = tower.last().unwrap().first().unwrap().clone();
+    tower.pop();
+    tower.iter().rev().for_each(|current_row| {
+        current_diff_end = current_row.last().unwrap() + current_diff_end;
+        current_diff_start = current_row.first().unwrap() - current_diff_start;
+    });
+    (current_diff_start, current_diff_end)
+}
+
 pub fn solve() -> SolutionPair {
-    todo!()
+    let data: Vec<Vec<isize>> = read_to_string("input/day9")
+        .unwrap()
+        .lines()
+        .map(|l| l.split(" ").map(|v| v.parse::<isize>().unwrap()).collect())
+        .collect();
+    let mut part_1 = 0;
+    let mut part_2 = 0;
+    for d in data {
+        let (this_2, this_1) = extrapolate_next_value(d);
+        part_1 += this_1;
+        part_2 += this_2;
+    }
+    (Solution::Int(part_1), Solution::Int(part_2))
 }
